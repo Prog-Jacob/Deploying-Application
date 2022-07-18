45141701af1f001540e7a2010053d8e2a64ee30c
Prog-Jacob <ahmed.abdelaziz.gm@gmail.com>
2022-07-18 13:32:34 +0200

    commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -4,6 +4,7 @@ export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=password
 export POSTGRES_HOST=udegram.c6woayur4pym.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=udegram
+export PORT=5432
 export AWS_BUCKET=arn:aws:s3:::udegram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
