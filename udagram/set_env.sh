f5c581fb0d0c1b13b84d051080201ab31293e0da
Prog-Jacob <ahmed.abdelaziz.gm@gmail.com>
2022-07-19 18:03:51 +0200

    commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,2 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=password
+export POSTGRES_HOST=udegram.c6woayur4pym.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udegram
+export PORT=5432
+export AWS_BUCKET=arn:aws:s3:::udegram
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://udagram-api-env.eba-thtgppxp.us-east-1.elasticbeanstalk.com
